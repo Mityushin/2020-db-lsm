82e3c56524194288cc8c874af06b82b69182e782
Dmitry Mityushin <DAnMityushin@sberbank.ru>
2021-05-22 14:21:53 +0300

    feature/stage-1 override range and get methods for more effective and fast processing

--- src/main/java/ru/mail/polis/mityushin/InMemoryBasedOnNavigableSetDAO.java
+++ src/main/java/ru/mail/polis/mityushin/InMemoryBasedOnNavigableSetDAO.java
@@ -1,6 +1,7 @@
 package ru.mail.polis.mityushin;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import ru.mail.polis.DAO;
 import ru.mail.polis.Record;
 
@@ -8,6 +9,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Iterator;
 import java.util.NavigableSet;
+import java.util.NoSuchElementException;
 import java.util.TreeSet;
 
 /**
@@ -28,7 +30,36 @@ public class InMemoryBasedOnNavigableSetDAO implements DAO {
     @NotNull
     @Override
     public Iterator<Record> iterator(@NotNull ByteBuffer from) throws IOException {
-        return set.tailSet(fromKey(from), true).iterator();
+        return set.tailSet(toRecord(from), true)
+                .iterator();
+    }
+
+    @NotNull
+    @Override
+    public Iterator<Record> range(@NotNull ByteBuffer from, @Nullable ByteBuffer to) throws IOException {
+        Record fromElement = toRecord(from);
+        NavigableSet<Record> subSet;
+        if (to == null) {
+            subSet = set.tailSet(fromElement, true);
+        } else {
+            subSet = set.subSet(fromElement, true, toRecord(to), false);
+        }
+        return subSet.iterator();
+    }
+
+    @NotNull
+    @Override
+    public ByteBuffer get(@NotNull ByteBuffer key) throws IOException, NoSuchElementException {
+        Record elementToFind = toRecord(key);
+        Record founded = set.ceiling(elementToFind);
+        if (founded == null || founded.compareTo(elementToFind) != 0) {
+            throw new NoSuchElementException();
+        }
+        ByteBuffer value = founded.getValue();
+        if (value == null) {
+            throw new NoSuchElementException();
+        }
+        return value;
     }
 
     @Override
@@ -41,7 +72,7 @@ public class InMemoryBasedOnNavigableSetDAO implements DAO {
 
     @Override
     public void remove(@NotNull ByteBuffer key) throws IOException {
-        set.remove(fromKey(key));
+        set.remove(toRecord(key));
     }
 
     @Override
@@ -70,7 +101,7 @@ public class InMemoryBasedOnNavigableSetDAO implements DAO {
                 '}';
     }
 
-    private Record fromKey(ByteBuffer key) {
+    private Record toRecord(ByteBuffer key) {
         return Record.of(key, EMPTY_VALUE);
     }
 }
--- src/main/java/ru/mail/polis/mityushin/InMemoryBasedOnSortedMapDAO.java
+++ src/main/java/ru/mail/polis/mityushin/InMemoryBasedOnSortedMapDAO.java
@@ -33,6 +33,30 @@ public class InMemoryBasedOnSortedMapDAO implements DAO {
                 .iterator();
     }
 
+    @NotNull
+    @Override
+    public Iterator<Record> range(@NotNull ByteBuffer from, @Nullable ByteBuffer to) throws IOException {
+        SortedMap<ByteBuffer, Record> subMap;
+        if (to == null) {
+            subMap = map.tailMap(from);
+        } else {
+            subMap = map.subMap(from, to);
+        }
+        return subMap
+                .values()
+                .iterator();
+    }
+
+    @NotNull
+    @Override
+    public ByteBuffer get(@NotNull ByteBuffer key) throws IOException, NoSuchElementException {
+        Record record = map.get(key);
+        if (record == null) {
+            throw new NoSuchElementException();
+        }
+        return record.getValue();
+    }
+
     @Override
     public void upsert(@NotNull ByteBuffer key, @NotNull ByteBuffer value) throws IOException {
         map.compute(key, (k, v) -> Record.of(key, value));
